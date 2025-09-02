ee9f1ffaa0ba74a070e155f5e7cf1515528cd152
bdel09 <bmcdelbarrio@gmail.com>
2025-09-02 11:39:26 -0400

    Update student.py

--- student.py
+++ student.py
@@ -2,14 +2,12 @@
 #Email: Benjamin.delbarrio31@myhunter.cuny.edu
 
 import turtle
-wn= turtle.Screen()
 t=turtle.Turtle()
 t.color('blue')
 t.speed(0)
 for i in range(50):
    for j in range(5):
-    t.forward(100)
-    t.right(72)
+       t.forward(100)
+       t.right(72)
    t.right(70)
-t.done()
-wn.exitonclick()
+
