c6b5e070e744122ae840f021c7d26e1639428390
bdel09 <bmcdelbarrio@gmail.com>
2025-09-02 12:30:32 -0400

    Update student.py

--- student.py
+++ student.py
@@ -4,10 +4,10 @@
 import turtle
 
 t = turtle.Turtle()
-t.color('blue')
 t.speed(0)
+t.color('blue')
 
-for i in range(49):
+for i in range(50):
     for j in range(5):
         t.forward(100)
         t.right(72)
